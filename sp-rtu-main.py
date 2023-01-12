7a3019d9a3ec67e25b9d893d7c7f1bd30e808cb9
Daryll Magsombol <daryllmagsombol@Darylls-MacBook-Air.local>
2023-01-13 03:11:20 +0800

    fixed errors, added checking and fail safe, logs

--- sp-rtu-main.py
+++ sp-rtu-main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from operator import attrgetter
 import csv
 import logging
 import time
@@ -33,7 +34,6 @@ def compute_interval(no_of_devices, interval):
 # main
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("device", type=str, help="Modbus device")
     argparser.add_argument("--stopbits", type=int, default=1, help="Stop bits")
     argparser.add_argument("--parity", type=str, default="N",
                            choices=["N", "E", "O"], help="Parity")
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     log_file_path = './sdm.log'
     config_file_path = './config.json'
     generate_report_file_path = './generated-reports.csv'
-    sleep_secs = 10
+    sleep_secs = 20
 
     # initialize logger
     logging.basicConfig(filename=log_file_path,
@@ -91,60 +91,76 @@ if __name__ == "__main__":
     # main program to get SDM datas
     power_meters = data["devices"]
     while (unli_loop == True):
-        for power_meter in power_meters:
-            meter = None
-            meter_type = power_meter["type"]
-            name = power_meter["name"]
-            slave_id = power_meter["slave_id"]
+        try:
+            for power_meter in power_meters:
+                meter = None
+                serial_path = data["serial_path"] if data.get(
+                    "serial_path") else '/dev/ttyUSB0'
+                meter_type = power_meter["type"]
+                name = power_meter["name"]
+                slave_id = power_meter["slave_id"]
+                baud_value = power_meter["baud_rate"] if power_meter.get(
+                    "baud_rate") else args.baud
 
-            print(meter_type, ' detected',
-                  'Slave ID: ', slave_id)
+                print(meter_type, ' detected',
+                      'Slave ID: ', slave_id)
 
-            if power_meter["type"].upper() == "SDM120":
-                meter = sdm_modbus.SDM120(
-                    device=args.device,
-                    stopbits=args.stopbits,
-                    parity=args.parity,
-                    baud=args.baud,
-                    timeout=args.timeout,
-                    unit=power_meter["slave_id"]
-                )
-            elif power_meter["type"].upper() == "SDM630":
-                meter = sdm_modbus.SDM630(
-                    device=args.device,
-                    stopbits=args.stopbits,
-                    parity=args.parity,
-                    baud=args.baud,
-                    timeout=args.timeout,
-                    unit=power_meter["slave_id"]
-                )
-            else:
-                print(meter_type, 'is not supported')
-                logging.info(
-                    "Meter Type: %s, Name: %s, ID: %d is not supported" % (meter_type, name, slave_id))
-                continue
-
-            if meter and meter.connected():
-                print("Modbus Detected! : ", meter)
-
-                meter_data = meter.read_all()
-
-                if (meter_data == {} or meter_data is None):
-                    logging.info(
-                        "Meter Type: %s, Name: %s, ID: %d is not detected or no data received" % (meter_type, name, slave_id))
+                if power_meter["type"].upper() == "SDM120":
+                    meter = sdm_modbus.SDM120(
+                        device=serial_path,
+                        stopbits=args.stopbits,
+                        parity=args.parity,
+                        baud=baud_value,
+                        timeout=args.timeout,
+                        unit=power_meter["slave_id"]
+                    )
+                elif power_meter["type"].upper() == "SDM630":
+                    meter = sdm_modbus.SDM630(
+                        device=serial_path,
+                        stopbits=args.stopbits,
+                        parity=args.parity,
+                        baud=baud_value,
+                        timeout=args.timeout,
+                        unit=power_meter["slave_id"]
+                    )
                 else:
+                    print(meter_type, 'is not supported')
                     logging.info(
-                        "Meter Type: %s, Name: %s, ID: %d has sent data" % (meter_type, name, slave_id))
-                    writer.writerow(
-                        {"name": power_meter["name"], "sdm_type": power_meter["type"], "datetime": datetime.now(), ** meter_data})
+                        "Meter Type: %s, Name: %s, ID: %d is not supported" % (meter_type, name, slave_id))
+                    continue
 
-            else:
-                print("Modbus not detected : ", meter)
-                logging.error(
-                    "Meter Type: %s, Name: %s, ID: %d can't be connected" % (meter_type, name, slave_id))
+                if meter and meter.connected():
+                    print("Modbus Detected! : ", meter)
+                    filtered_meter_data = {}
+                    meter_data = meter.read_all()
 
-            # time.sleep(2)
-            # meter_data = json.dumps(meter.read_all(scaling=True), indent=4)
+                    print(meter_data)
+
+                    if bool(meter_data):
+                        filtered_meter_data = {key: meter_data[key] for key in ['voltage', 'current', 'power_active', 'power_apparent', 'power_reactive', 'power_factor', 'phase_angle', 'frequency', 'import_energy_active', 'export_energy_active', 'import_energy_reactive', 'export_energy_reactive', 'total_demand_power_active',
+                                                                                'maximum_total_demand_power_active', 'import_demand_power_active', 'maximum_import_demand_power_active', 'export_demand_power_active', 'maximum_export_demand_power_active', 'total_demand_current', 'maximum_total_demand_current', 'total_energy_active', 'total_energy_reactive']}
+                        message = "[SUCCESS] Meter Type: %s, Name: %s, ID: %d has sent data" % (
+                            meter_type, name, slave_id)
+                        print(message)
+                        logging.info(message)
+                        writer.writerow(
+                            {"name": power_meter["name"], "sdm_type": power_meter["type"], "datetime": datetime.now(), **filtered_meter_data})
+                    else:
+                        message = "[ERR] Meter Type: %s, Name: %s, ID: %d is not detected or no data received" % (
+                            meter_type, name, slave_id)
+                        print(message)
+                        logging.error(message)
+                else:
+                    print("Modbus not detected : ", meter)
+                    logging.error(
+                        "Meter Type: %s, Name: %s, ID: %d can't be connected" % (meter_type, name, slave_id))
+
+                # time.sleep(2)
+                # meter_data = json.dumps(meter.read_all(scaling=True), indent=4)
+        except Exception as e_message:
+            print('[ERR', e_message)
+            logging.error('[ERR', e_message)
+
         interval = compute_interval(len(power_meters), sleep_secs)
         print("Sleeping for %s secs..." % interval)
         time.sleep(interval)
